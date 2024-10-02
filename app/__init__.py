fd68532ad02ae23fd019892d8fae4e8c677e50ce
SibiSM <sibichakravarthy888@gmail.com>
2024-10-02 19:23:21 -0400

    new

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+import logging
 
 db = SQLAlchemy()
 
@@ -15,4 +20,15 @@ def create_app():
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return app
+    # Integrate Application Insights
+    connection_string = "InstrumentationKey=710443bc-9d4e-43a4-9674-7d889501eaa5;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=b2256d6c-2a0b-4da7-8123-3cfab3279323"
+
+    # Logger configuration for Azure Application Insights
+    logger = logging.getLogger(__name__)
+    logger.addHandler(AzureLogHandler(connection_string=connection_string))
+    logger.setLevel(logging.INFO)
+
+    # Middleware for tracking Flask requests
+    FlaskMiddleware(app, exporter=AzureExporter(connection_string=connection_string), sampler=ProbabilitySampler(1.0))
+
+    return app, logger
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app()
+app, logger = create_app()  # Unpack the app and logger
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True, port=8000)
